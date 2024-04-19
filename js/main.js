820a816d9374863505c65f3f39858559140b87d4
Shande Moura <shandeba@yahoo.com.br>
2024-04-18 21:52:49 -0300

    Contact, Footer, Themes and Scrolls

--- js/main.js
+++ js/main.js
@@ -49,3 +49,59 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
     })
   })
 })
+
+// Website dark/light theme
+const themeBtn = document.querySelector(".theme-btn")
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle('dark-theme')
+  themeBtn.classList.toggle('sun')
+  localStorage.setItem("saved-theme", getCurrentTheme())
+  localStorage.setItem("saved-icon", getCurrentIcon())
+})
+
+const getCurrentTheme = () => document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+const getCurrentIcon = () => themeBtn.classList.contains('sun') ? 'sun' : 'moon';
+
+const savedTheme = localStorage.getItem('saved-theme');
+const savedIcon = localStorage.getItem('saved-icon');
+
+if(savedTheme){
+  document.body.classList[savedTheme === "dark" ?  "add" : "remove"]('dark-theme');
+  themeBtn.classList[savedIcon === "sun" ?  "add" : "remove"]('sun')
+} else {
+  document.body.classList.add('light-theme');
+  themeBtn.classList.add('fa-sun');
+}
+
+
+// Scrool to Top Button
+const scrollTopBtn = document.querySelector(".scrollToTop-btn")
+
+window.addEventListener("scroll", function(){
+  scrollTopBtn.classList.toggle("active", window.scrollY > 500)
+})
+
+scrollTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
+})
+
+
+// Navigation menu ites active
+window.addEventListener('scroll', () => {
+  const sections = document.querySelectorAll('section')
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    let sectionHeight = current.offsetHeight
+    let sectionTop = current.offsetTop - 50
+    let id = current.getAttribute( 'id' )
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector(".nav-items a[href*=" + id + "]").classList.add('active');
+      } else{
+        document.querySelector(".nav-items a[href*=" + id + "]").classList.remove('active');
+      }
+  })
+})
